627263982381ab3db5a1959712621e272a5490c0
Bakhshali <tu78rfrfu@code.edu.az>
2022-03-05 13:01:43 +0400

    Task 1 added

---
+++ Task1(LOOP)/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task1_LOOP_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] brand = new string[] {"Mercedes" , "BMW" , "Audi", "Nissan", "Porche", "Lambo", "GMC" };
+            for (int i = 0; i < brand.Length; i++)
+                if (brand[i].Length > 5)
+                {
+                Console.WriteLine(brand[i]);
+                    
+            }
+        }
+    }
+}
